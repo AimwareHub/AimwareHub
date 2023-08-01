84d3a630eacad2cfb1ebf53bb632935c0b86fd6a
AimwareHub <141087523+AimwareHub@users.noreply.github.com>
2023-07-31 21:24:10 -0600

    Update .lua

--- .lua
+++ .lua
@@ -1,28 +1,28 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "💢 Aimware's Hoopz GUI 💢",
-   LoadingTitle = "Loading",
-   LoadingSubtitle = "Aimware's Hoopz",
+   Name = "Rayfield Example Window",
+   LoadingTitle = "Rayfield Interface Suite",
+   LoadingSubtitle = "by Sirius",
    ConfigurationSaving = {
-      Enabled = false,
+      Enabled = true,
       FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Aimware Hub"
+      FileName = "Big Hub"
    },
    Discord = {
-      Enabled = true,
-      Invite = "JBQUk38XWh", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = false -- Set this to false to make them join the discord every time they load it up
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
-   KeySystem = true, -- Set this to true to use our key system
+   KeySystem = false, -- Set this to true to use our key system
    KeySettings = {
-      Title = "Aimware's Hoopz",
+      Title = "Untitled",
       Subtitle = "Key System",
-      Note = "Join the discord server for key",
+      Note = "No method of obtaining the key is provided",
       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"loadstring(game:HttpGet("https://raw.githubusercontent.com/AimwareHub/Aimwarehubkey/main/.lua"))();"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
